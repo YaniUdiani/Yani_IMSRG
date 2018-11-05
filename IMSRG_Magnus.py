c3b98e52eb2ae01863183d2e48b483edae602da9
Omokuyani Udiani <ocudiani@ncsu.edu>
2018-11-05 11:47:52 -0500

    My cleaned up Magnus

--- IMSRG_Magnus.py
+++ IMSRG_Magnus.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 #------------------------------------------------------------------------------
-# imsrg_pairing.py
+# IMSRG_Magnus.py
 #
-# author:   H. Hergert 
-# version:  1.5.0
-# date:     Dec 6, 2016
+# author:   Yani Udiani
+# Adapted from Heiko's IMSRG
+# date:     Nov, 1, 2018
 # 
-# tested with Python v2.7
+# tested with Python 3
 # 
 # Solves the pairing model for four particles in a basis of four doubly 
-# degenerate states by means of an In-Medium Similarity Renormalization 
+# degenerate states by means of an In-Medium Similarity Renormalization using the Magnus expansion
 # Group (IMSRG) flow.
 #
 #------------------------------------------------------------------------------
@@ -27,12 +27,8 @@ import matplotlib.pyplot as plt # Plotting stuff
 #-----------------------------------------------------------------------------------
 # basis and index functions
 #-----------------------------------------------------------------------------------
-Higher_Order=[]
-THigher_Order=[]
 flow=[]
 energy=[]
-yum=[]
-yumyum=[]
 def construct_basis_2B(holes, particles):
   basis = []
   for i in holes:
@@ -525,30 +521,25 @@ def eta_wegner(f, Gamma, user_data):
 
 
 #-----------------------------------------------------------------------------------
-# derivatives 
+# derivatives : My changes start here
 #-----------------------------------------------------------------------------------
   
 def special_commutator(x,y,user_data): # takes in either 1 or 2 body matrices to output 0,1, and 2 body commutators
     dim1B     = user_data["dim1B"]
     holes     = user_data["holes"]
-    particles = user_data["particles"]
-    bas2B     = user_data["bas2B"]
+    particles = user_data["particles"]  
     idx2B     = user_data["idx2B"]
-    basph2B   = user_data["basph2B"]
-    idxph2B   = user_data["idxph2B"]
-    occB_2B   = user_data["occB_2B"]
-    occC_2B   = user_data["occC_2B"]
-    occphA_2B = user_data["occphA_2B"]
-    states=particles+holes
+    states=particles+holes #combine particles and holes for easy indexing
+    #Initializations 
     Output_1B=np.zeros((dim1B,dim1B))
     Output_2B=np.zeros((dim1B**2,dim1B**2))
     Output_0B=0.0
-    # The remaining lines of the function hardcode the commutators. I will convert some of these to matrix multiplication soon
+    # The remaining lines of the function brute force the commutators. I will convert some of these to matrix multiplication once I fix my bugs :)
     if(np.size(x)== len(states)**2 and np.size(y)== len(states)**2):#1B-1B
-        Output_1B=Output_1B+commutator(x,y)
+        Output_1B+=commutator(x,y)#regular commutator
         for i in holes:# 0B Correction
             for j in particles:
-                Output_0B=Output_0B+x[i,j]*y[j,i]-x[j,i]*y[i,j]
+                Output_0B+=x[i,j]*y[j,i]-x[j,i]*y[i,j]
                 
     if(np.size(x)== len(states)**4 and np.size(y)== len(states)**2):#2B-1B 
         for i in states:
@@ -556,14 +547,14 @@ def special_commutator(x,y,user_data): # takes in either 1 or 2 body matrices to
                 for a in holes:
                     for b in particles:
                         #Output_1B[i,j]+=-(int(a in holes)-int(b in holes))*y[i,j]*x[idx2B[(b,i)],idx2B[(a,j)]]
-                        Output_1B[i,j]=Output_1B[i,j]-(y[i,j]*x[idx2B[(b,i)],idx2B[(a,j)]]-y[i,j]*x[idx2B[(a,i)],idx2B[(b,j)]])
+                        Output_1B[i,j]+=-(y[i,j]*x[idx2B[(b,i)],idx2B[(a,j)]]-y[i,j]*x[idx2B[(a,i)],idx2B[(b,j)]])
                         
         for i in states:
             for j in states:
                 for k in states:
                     for l in states:
                         for a in states:
-                            Output_2B[idx2B[(i,j)],idx2B[(k,l)]]=Output_2B[idx2B[(i,j)],idx2B[(k,l)]]-( (y[i,a]*x[idx2B[(a,j)],idx2B[(k,l)]]-y[j,a]*x[idx2B[(a,i)],idx2B[(k,l)]]) - (y[a,k]*x[idx2B[(i,j)],idx2B[(a,l)]]-y[a,l]*x[idx2B[(i,j)],idx2B[(a,k)]]))
+                            Output_2B[idx2B[(i,j)],idx2B[(k,l)]]+=-( (y[i,a]*x[idx2B[(a,j)],idx2B[(k,l)]]-y[j,a]*x[idx2B[(a,i)],idx2B[(k,l)]]) - (y[a,k]*x[idx2B[(i,j)],idx2B[(a,l)]]-y[a,l]*x[idx2B[(i,j)],idx2B[(a,k)]]))
                             
     if(np.size(x)== len(states)**2 and np.size(y)== len(states)**4):#1B-2B 
         for i in states:
@@ -571,14 +562,14 @@ def special_commutator(x,y,user_data): # takes in either 1 or 2 body matrices to
                 for a in holes:
                     for b in particles:
                         #Output_1B[i,j]+=(int(a in holes)-int(b in holes))*x[i,j]*y[idx2B[(b,i)],idx2B[(a,j)]]
-                        Output_1B[i,j]=Output_1B[i,j]-(x[i,j]*y[idx2B[(b,i)],idx2B[(a,j)]]-x[i,j]*y[idx2B[(a,i)],idx2B[(b,j)]])
+                        Output_1B[i,j]+=-(x[i,j]*y[idx2B[(b,i)],idx2B[(a,j)]]-x[i,j]*y[idx2B[(a,i)],idx2B[(b,j)]])
                         
         for i in states:
             for j in states:
                 for k in states:
                     for l in states:
                         for a in states:
-                            Output_2B[idx2B[(i,j)],idx2B[(k,l)]]=Output_2B[idx2B[(i,j)],idx2B[(k,l)]]+( (x[i,a]*y[idx2B[(a,j)],idx2B[(k,l)]]-x[j,a]*y[idx2B[(a,i)],idx2B[(k,l)]]) - (x[a,k]*y[idx2B[(i,j)],idx2B[(a,l)]]-x[a,l]*y[idx2B[(i,j)],idx2B[(a,k)]])) 
+                            Output_2B[idx2B[(i,j)],idx2B[(k,l)]]+= (x[i,a]*y[idx2B[(a,j)],idx2B[(k,l)]]-x[j,a]*y[idx2B[(a,i)],idx2B[(k,l)]]) - (x[a,k]*y[idx2B[(i,j)],idx2B[(a,l)]]-x[a,l]*y[idx2B[(i,j)],idx2B[(a,k)]]) 
 
     if(np.size(x)== len(states)**4 and np.size(y)== len(states)**4):# 2B-2B
         for i in holes:
@@ -586,13 +577,13 @@ def special_commutator(x,y,user_data): # takes in either 1 or 2 body matrices to
                 for k in particles:
                     for l in particles:
                         #Output_0B+=0.25*(int(i in holes)*int(j in holes)*(1-int(k in holes))*(1-int(l in holes)))*(x[idx2B[(i,j)],idx2B[(k,l)]]*y[idx2B[(k,l)],idx2B[(i,j)]]-x[idx2B[(k,l)],idx2B[(i,j)]]*y[idx2B[(i,j)],idx2B[(k,l)]])
-                        Output_0B=Output_0B+0.25*(x[idx2B[(i,j)],idx2B[(k,l)]]*y[idx2B[(k,l)],idx2B[(i,j)]]-x[idx2B[(k,l)],idx2B[(i,j)]]*y[idx2B[(i,j)],idx2B[(k,l)]])                       
+                        Output_0B+=0.25*(x[idx2B[(i,j)],idx2B[(k,l)]]*y[idx2B[(k,l)],idx2B[(i,j)]]-x[idx2B[(k,l)],idx2B[(i,j)]]*y[idx2B[(i,j)],idx2B[(k,l)]])                       
         for i in states:
             for j in states:
                 for a in states:
                     for b in states:
                         for c in states:
-                            Output_1B[i,j]=Output_1B[i,j]+0.5*( x[idx2B[(c,i)],idx2B[(a,b)]]*y[idx2B[(a,b)],idx2B[(c,j)]] - x[idx2B[(a,b)],idx2B[(c,j)]]*y[idx2B[(c,i)],idx2B[(a,b)]] )*((1-int(a in holes))*(1-int(b in holes))*int(c in holes)+(1-int(c in holes))*int(a in holes)*int(b in holes))
+                            Output_1B[i,j]+=0.5*( x[idx2B[(c,i)],idx2B[(a,b)]]*y[idx2B[(a,b)],idx2B[(c,j)]] - x[idx2B[(a,b)],idx2B[(c,j)]]*y[idx2B[(c,i)],idx2B[(a,b)]] )*((1-int(a in holes))*(1-int(b in holes))*int(c in holes)+(1-int(c in holes))*int(a in holes)*int(b in holes))
                             
         for i in states:
             for j in states:
@@ -600,13 +591,14 @@ def special_commutator(x,y,user_data): # takes in either 1 or 2 body matrices to
                     for l in states:
                         for a in states:
                             for b in states:
-                                Output_2B[idx2B[(i,j)],idx2B[(k,l)]]=Output_2B[idx2B[(i,j)],idx2B[(k,l)]]+(0.5*(x[idx2B[(i,j)],idx2B[(a,b)]]*y[idx2B[(a,b)],idx2B[(k,l)]]-x[idx2B[(a,b)],idx2B[(k,l)]]*y[idx2B[(i,j)],idx2B[(a,b)]])*(1-int(a in holes)-int(b in holes))+(int(a in holes)-int(b in holes))*(x[idx2B[(a,i)],idx2B[(b,k)]]*y[idx2B[(b,j)],idx2B[(a,l)]]-x[idx2B[(a,j)],idx2B[(b,k)]]*y[idx2B[(b,i)],idx2B[(a,l)]]-x[idx2B[(a,i)],idx2B[(b,l)]]*y[idx2B[(b,j)],idx2B[(a,k)]]+x[idx2B[(a,j)],idx2B[(b,l)]]*y[idx2B[(b,i)],idx2B[(a,k)]]))
+                                Output_2B[idx2B[(i,j)],idx2B[(k,l)]]+=(0.5*(x[idx2B[(i,j)],idx2B[(a,b)]]*y[idx2B[(a,b)],idx2B[(k,l)]]-x[idx2B[(a,b)],idx2B[(k,l)]]*y[idx2B[(i,j)],idx2B[(a,b)]])*(1-int(a in holes)-int(b in holes))+(int(a in holes)-int(b in holes))*(x[idx2B[(a,i)],idx2B[(b,k)]]*y[idx2B[(b,j)],idx2B[(a,l)]]-x[idx2B[(a,j)],idx2B[(b,k)]]*y[idx2B[(b,i)],idx2B[(a,l)]]-x[idx2B[(a,i)],idx2B[(b,l)]]*y[idx2B[(b,j)],idx2B[(a,k)]]+x[idx2B[(a,j)],idx2B[(b,l)]]*y[idx2B[(b,i)],idx2B[(a,k)]]))
     
     return Output_0B, Output_1B, Output_2B   
 
 def RHS_Cal(Omega_1B,Omega_2B,Eta_1B,Eta_2B, user_data):# Generates right hand side of flow equation to be fed to diffy solver to obtain new Omega
-    bn2 = user_data["bn2"]
-    RHS_0B=0. #Initialize Hamilitonian
+    bn2 = user_data["bn2"]#import Bernoulli numbers
+    #Initializations
+    RHS_0B=0. 
     RHS_1B=np.zeros_like(Omega_1B)
     RHS_2B=np.zeros_like(Omega_2B)
     special_commutator=user_data["special_commutator"]
@@ -615,78 +607,72 @@ def RHS_Cal(Omega_1B,Omega_2B,Eta_1B,Eta_2B, user_data):# Generates right hand s
             nth_2B=Eta_2B #Initial commutators defined to be Eta
             nth_1B=Eta_1B
             nth_0B=0.0
-            #THigher_Order.append(Eta_1B)
         else:
             # The next lines aggregate the current 0B, 1B, and 2B nth commutators from the Magnus expansion
             nth_0B=special_commutator(Omega_1B,nth_1B, user_data)[0]
             nth_0B=nth_0B+special_commutator(Omega_2B,nth_2B,user_data)[0]
             
             nth_1B=special_commutator(Omega_1B,nth_1B, user_data)[1]
-            nth_1B=nth_1B+special_commutator(Omega_1B,nth_2B,user_data)[1]
-            nth_1B=nth_1B+special_commutator(Omega_2B,nth_1B, user_data)[1]
-            nth_1B=nth_1B+special_commutator(Omega_2B,nth_2B, user_data)[1]
+            nth_1B+=special_commutator(Omega_1B,nth_2B,user_data)[1]
+            nth_1B+=special_commutator(Omega_2B,nth_1B, user_data)[1]
+            nth_1B+=special_commutator(Omega_2B,nth_2B, user_data)[1]
             
             nth_2B=special_commutator(Omega_1B,nth_2B, user_data)[2]
-            nth_2B=nth_2B+special_commutator(Omega_2B,nth_1B, user_data)[2]
-            nth_2B=nth_2B+special_commutator(Omega_2B,nth_2B, user_data)[2]
+            nth_2B+=special_commutator(Omega_2B,nth_1B, user_data)[2]
+            nth_2B+=special_commutator(Omega_2B,nth_2B, user_data)[2]
             
         # Next lines are from recursion relation from Baker–Campbell–Hausdorff formula modified including Bernoulli #s
         
         shiftymatrix_0B=nth_0B/np.math.factorial(n)
         shiftymatrix_1B=nth_1B/np.math.factorial(n)
         shiftymatrix_2B=nth_2B/np.math.factorial(n)
-        #Higher_Order.append(LA.norm(shiftymatrix_1B))
-        #THigher_Order.append(n)
         if ((LA.norm(shiftymatrix_1B)+ LA.norm(shiftymatrix_2B))<1e-10):
             break        
-        RHS_0B=RHS_0B+bn2[n]*shiftymatrix_0B
-        RHS_1B=RHS_1B+bn2[n]*shiftymatrix_1B
-        RHS_2B=RHS_2B+bn2[n]*shiftymatrix_2B
-
-  #      print(LA.norm(shiftymatrix_2B), n)
+        RHS_0B+=bn2[n]*shiftymatrix_0B
+        RHS_1B+=bn2[n]*shiftymatrix_1B
+        RHS_2B+=bn2[n]*shiftymatrix_2B
+        
     return RHS_0B, RHS_1B, RHS_2B
 
 def Transformed_Ham(Omega_1B, Omega_2B, user_data):# Generates new Hamiltonian by performing BCH expansion
-    bn2 = user_data["bn2"]
-    E=user_data["E"]
-    f=user_data["f"]
-    Gamma=user_data["Gamma"]
-    H_0B=0. #Initialize Hamilitonian
+    bn2 = user_data["bn2"]#import Bernoulli numbers
+    E=user_data["E"]#Import initial energy
+    f=user_data["f"]#import initial one body hamiltonian
+    Gamma=user_data["Gamma"]#import initial two body hamiltonian
+    #Initializations
+    H_0B=0.
     H_1B=np.zeros_like(f)
     H_2B=np.zeros_like(Gamma)
     special_commutator=user_data["special_commutator"] # I really don't need this. Come back to clean it up later
     for n in range(len(bn2)):
         if n==0: 
-            nth_2B=Gamma #Initial commutators defined to be Eta
+            nth_2B=Gamma #Initial commutators defined to be initial Hamiltonian
             nth_1B=f
             nth_0B=E
         else:
             # The next lines aggregate the current 0B, 1B, and 2B nth commutators from the Magnus expansion
             nth_0B=special_commutator(Omega_1B,nth_1B, user_data)[0]
-            nth_0B=nth_0B+special_commutator(Omega_2B,nth_2B,user_data)[0]
+            nth_0B+=special_commutator(Omega_2B,nth_2B,user_data)[0]
             
             nth_1B=special_commutator(Omega_1B,nth_1B, user_data)[1]
-            nth_1B=nth_1B+special_commutator(Omega_1B,nth_2B,user_data)[1]
-            nth_1B=nth_1B+special_commutator(Omega_2B,nth_1B, user_data)[1]
-            nth_1B=nth_1B+special_commutator(Omega_2B,nth_2B, user_data)[1]
+            nth_1B+=special_commutator(Omega_1B,nth_2B,user_data)[1]
+            nth_1B+=special_commutator(Omega_2B,nth_1B, user_data)[1]
+            nth_1B+=special_commutator(Omega_2B,nth_2B, user_data)[1]
             
             nth_2B=special_commutator(Omega_1B,nth_2B, user_data)[2]
-            nth_2B=nth_2B+special_commutator(Omega_2B,nth_1B, user_data)[2]
-            nth_2B=nth_2B+special_commutator(Omega_2B,nth_2B, user_data)[2]
+            nth_2B+=special_commutator(Omega_2B,nth_1B, user_data)[2]
+            nth_2B+=special_commutator(Omega_2B,nth_2B, user_data)[2]
             
         # Next lines are from recursion relation from Baker–Campbell–Hausdorff formula modified including Bernoulli #s
         
         shiftymatrix_0B=nth_0B/np.math.factorial(n)
         shiftymatrix_1B=nth_1B/np.math.factorial(n)
         shiftymatrix_2B=nth_2B/np.math.factorial(n)
-        #THigher_Order.append(n)
-        #Higher_Order.append(LA.norm(shiftymatrix_1B))
         if ((LA.norm(shiftymatrix_1B)+LA.norm(shiftymatrix_2B))<1e-10):
             break
-        H_0B=H_0B+shiftymatrix_0B
-        H_1B=H_1B+shiftymatrix_1B
-        H_2B=H_2B+shiftymatrix_2B
-  #      print(LA.norm(shiftymatrix_2B), n)
+        H_0B+=shiftymatrix_0B
+        H_1B+=shiftymatrix_1B
+        H_2B+=shiftymatrix_2B
     return H_0B, H_1B, H_2B
 #-----------------------------------------------------------------------------------
 # derivative wrapper
@@ -710,22 +696,26 @@ def derivative_wrapper(t, y, user_data):
 
   dim1B = user_data["dim1B"]
   dim2B = dim1B*dim1B
-  calc_eta  = user_data["calc_eta"]
-  calc_rhs  = user_data["calc_rhs"]    #right hand side of zero body piece
+  calc_eta  = user_data["calc_eta"]# Function to calculate generator
+  calc_rhs  = user_data["calc_rhs"]    #function to calculate RHS
+  
   # extract operator pieces from solution vector
   Omega_0B,Omega_1B, Omega_2B = get_operator_from_y(y, dim1B, dim2B)
   #print(Omega_2B) 
-
+  
+  #Transform Hamiltonian using Omega
   Transformed_Hamiltonian=Transformed_Ham(Omega_1B, Omega_2B, user_data)
   
   # calculate the generator
   Eta_1B, Eta_2B = calc_eta(Transformed_Hamiltonian[1], Transformed_Hamiltonian[2], user_data)
+  
   # calculate the right-hand side
   dOmega_0B, dOmega_1B, dOmega_2B = calc_rhs(Omega_1B,Omega_2B,Eta_1B,Eta_2B, user_data)
+  
   # convert derivatives into linear array
   dy   = np.append([dOmega_0B], np.append(reshape(dOmega_1B, -1), reshape(dOmega_2B, -1)))
   # share data
-  user_data["dE"] = dOmega_0B
+  user_data["dE"] = dOmega_0B #storing dOmega_0B/ds in dE
   user_data["eta_norm"] = np.linalg.norm(Eta_1B,ord='fro')+np.linalg.norm(Eta_2B,ord='fro')
   
   return dy
@@ -860,12 +850,12 @@ def main():
   # grab delta and g from the command line
   delta      =1
   g          = 0.5
-  # this defines the reference state
-
-  holes=[0,1]
-  particles=[2,3]
+  # Reference state:
+  holes=[0,1,2,3]
+  particles=[4,5,6,7]
   # setup shared data
   dim1B = len(holes)+len(particles)
+  dim2B = dim1B*dim1B
   # basis definitions
   bas1B     = range(dim1B)
   bas2B     = construct_basis_2B(holes, particles)
@@ -881,7 +871,7 @@ def main():
   occC_2B   = construct_occupationC_2B(bas2B, occ1B)
 
   occphA_2B = construct_occupationA_2B(basph2B, occ1B)
-  bn2=Bernoulli_generator(16)#caveman coding for now
+  bn2=Bernoulli_generator(16)# Go up to 16 terms in expansion
 
   # store shared data in a dictionary, so we can avoid passing the basis
   # lookups etc. as separate parameters all the time
@@ -900,27 +890,29 @@ def main():
     "occC_2B":    occC_2B,
     "occphA_2B":  occphA_2B,
     "bn2": bn2,
-
-    "eta_norm":   0.0,                # variables for sharing data between ODE solver
-    "dE":         0.0,                # and main routine
-
-
-    "calc_eta":   eta_brillouin,          # specify the generator (function object)
+    # variables for sharing data between ODE solver
+    "eta_norm":   0.0,                #Initial norm of eta
+    "dE":         0.0,                # Initial dOmega/ds
+    "calc_eta":   eta_white,          # specify the generator (function object)
     "calc_rhs":   RHS_Cal,         # specify the right-hand side and truncation
-    "special_commutator": special_commutator
+    "special_commutator": special_commutator #commutator used to evaluate n body commuatators: 0 up to 2 body matrices
   }
 
   # set up initial Hamiltonian
   H1B, H2B = pairing_hamiltonian(delta, g, user_data)
 
-  E, f, Gamma = normal_order(H1B, H2B, user_data) 
+  E, f, Gamma = normal_order(H1B, H2B, user_data)#initial Hamiltonian
+  
   # Append Initial Hamiltonian to dictionary since that won't be changing in the Magnus expansion
   user_data["E"]= E
   user_data["f"]= f
   user_data["Gamma"]= Gamma
+  
+  #Initializations
   Initial_Omega0=0.
   Initial_Omega1=np.zeros((dim1B,dim1B))
   Initial_Omega2=np.zeros((dim1B**2,dim1B**2))
+  
   # reshape generator into a linear array (initial ODE vector)
   y0   = np.append([Initial_Omega0], np.append(reshape(Initial_Omega1, -1), reshape(Initial_Omega2, -1)))
 
@@ -941,12 +933,11 @@ def main():
   while solver.successful() and solver.t < sfinal:
     ys = solver.integrate(sfinal, step=True)
     
-    dim2B = dim1B*dim1B
-    Omega_0B,Omega_1B, Omega_2B = get_operator_from_y(ys, dim1B, dim2B)
-    E, f , Gamma=Transformed_Ham(Omega_1B, Omega_2B, user_data)
+    Omega_0B,Omega_1B, Omega_2B = get_operator_from_y(ys, dim1B, dim2B)#get Omegas
+    E, f , Gamma=Transformed_Ham(Omega_1B, Omega_2B, user_data)#get  Hamiltonian to use MBPT, and print out values
     
-    energy.append(E)
-    flow.append(solver.t)
+    energy.append(E)#append energy for plotting
+    flow.append(solver.t)#append flow parameter for plotting
 
 
     DE2 = calc_mbpt2(f, Gamma, user_data)
