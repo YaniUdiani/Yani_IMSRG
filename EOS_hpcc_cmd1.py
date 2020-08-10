3626b7e5969e4aa1715b60ede142920e0ebdd73a
YaniUdiani <ocudiani@ncsu.edu>
2020-08-10 14:58:37 -0400

    life is a struggle

--- EOS_hpcc_cmd1.py
+++ EOS_hpcc_cmd1.py
@@ -89,9 +89,9 @@ ds = args.step #obtain step size if requested
 
 #rhos = np.linspace(0.05, 0.2, num_points)
 
-rhos = np.array([0.02, 0.04, 0.06, 0.08, 0.1])
+#rhos = np.array([0.02, 0.04, 0.06, 0.08, 0.1])
 
-#rhos = np.array([0.12, 0.14, 0.16, 0.18, 0.2])
+rhos = np.array([0.12, 0.14, 0.16, 0.18, 0.2])
 
 num_points = len(rhos)
 
